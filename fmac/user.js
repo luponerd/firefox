148814c93270601548a527ea8cfbbdb8ecb955fb
luponerd <luponerdo@gmail.com>
2026-02-12 03:13:35 +0100

    Add files via upload

---
+++ fmac/user.js
@@ -0,0 +1,12 @@
+// Impostazioni per la gestione dei protocolli esterni
+user_pref("network.protocol-handler.expose-all", true);
+user_pref("network.protocol-handler.warn-external.ext+container", false);
+user_pref("network.protocol-handler.external.ext+container", true);
+
+// Disabilita il blocco dei popup (permette l'apertura di finestre durante il caricamento)
+user_pref("dom.disable_open_during_load", false);
+
+
+// istruzioni : digita about:support per sapere dov'è la Cartella del profilo (o "Profile Folder")
+// Copia il file: Chiudi completamente Firefox e trascina il file user.js
+// 
