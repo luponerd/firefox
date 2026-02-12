bc8c2adbc4b6c6565bd0df798cc489f1e5c3c6a4
luponerd <luponerdo@gmail.com>
2026-02-12 03:17:15 +0100

    Update user.js

--- fmac/user.js
+++ fmac/user.js
@@ -1,3 +1,4 @@
+// Digita Ctrl+S per scaricare il file da github
 // Impostazioni per la gestione dei protocolli esterni
 user_pref("network.protocol-handler.expose-all", true);
 user_pref("network.protocol-handler.warn-external.ext+container", false);
@@ -9,4 +10,5 @@ user_pref("dom.disable_open_during_load", false);
 
 // istruzioni : digita about:support per sapere dov'è la Cartella del profilo (o "Profile Folder")
 // Copia il file: Chiudi completamente Firefox e trascina il file user.js
+
 // 
